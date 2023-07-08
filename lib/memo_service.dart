dcdbbbf13cb8d23217034ceb212ccd10a3b972ed
leesangjun <prokjun@gmail.com>
2023-07-08 20:55:24 +0900

    완

--- lib/memo_service.dart
+++ lib/memo_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'main.dart';
@@ -9,29 +11,59 @@ class Memo {
   });
 
   String content;
+
+  Map toJson() {
+    return {'content': content};
+  }
+
+  factory Memo.fromJson(json) {
+    return Memo(content: json['content']);
+  }
 }
 
 // 메모 데이터는 모두 여기서 관리
 class MemoService extends ChangeNotifier {
-  List<Memo> memoList = [
-    Memo(content: '장보기 목록: 딸기, 포도'),
-    Memo(content: '세 매모2'),
-  ];
+  MemoService() {
+    loadMemoList();
+  }
+
+  List<Memo> memoList = [];
 
   createMemo({required String content}) {
     Memo memo = Memo(content: content);
     memoList.add(memo);
     notifyListeners();
+    saveMemoList();
   }
 
   updateMemo({required int index, required String content}) {
     Memo memo = memoList[index];
     memo.content = content;
     notifyListeners();
+    saveMemoList();
   }
 
   deleteMemo({required int index}) {
     memoList.removeAt(index);
     notifyListeners();
+    saveMemoList();
+  }
+
+  saveMemoList() {
+    List memoJsonList = memoList.map((memo) => memo.toJson()).toList();
+
+    String jsonString = jsonEncode(memoJsonList);
+
+    prefs.setString('memoList', jsonString);
+  }
+
+  loadMemoList() {
+    String? jsonString = prefs.getString('memoList');
+
+    if (jsonString == null) return;
+
+    List memoJsonList = jsonDecode(jsonString);
+
+    memoList = memoJsonList.map((json) => Memo.fromJson(json)).toList();
   }
 }
